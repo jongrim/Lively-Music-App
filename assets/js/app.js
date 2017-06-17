86e2616bc4619b3b6b773d4a307ec67b67f21aae
Jonathan Grim <jonjongrim@gmail.com>
2017-06-17 13:21:15 -0400

    Adds form toggling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,22 @@
 window.EVT = new EventEmitter2();
 
+var App = (function() {
+  let $advForm, $advFormToggleLink;
+
+  function init() {
+    $advForm = $('#advSearch');
+    $advFormToggleLink = $('#advSearchToggle');
+
+    $advFormToggleLink.on('click', toggleAdvSearchForm);
+  }
+
+  function toggleAdvSearchForm() {
+    $advForm.slideToggle();
+  }
+
+  EVT.on('init', init);
+})();
+
 $(document).ready(function() {
   EVT.emit('init');
 });
